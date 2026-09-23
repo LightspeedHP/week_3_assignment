6f044a0a8fca98e929ab6d9f15ef3e82b6fcd9be
Michael Akelemor <michaelakelemor@gmail.com>
2026-09-23 22:36:06 +0100

    bug fixes and improvements

--- app.js
+++ app.js
@@ -23,17 +23,27 @@ app.get('/todos', (req, res, next) => {
   }
 });
 
-//GET Single- Read
+//GET Active tasks
 app.get('/todos/active', (req, res, next) => {
   try {
-    const activeTodos = todos.filter((t) => t.completed);
+    const activeTodos = todos.filter((t) => !t.completed);
     res.status(200).json(activeTodos);
   } catch (error) {
     next(error)
   } 
 });
 
-//GET Active tasks
+//GET Inactive tasks
+app.get('/todos/completed', (req, res, next) => {
+  try {
+    const completed = todos.filter((t) => t.completed);
+    res.json(completed); // Custom Read!
+  } catch (error) {
+    next(error)
+  }
+});
+
+//GET Single- Read
 app.get('/todos/:id', (req, res, next) => {
   try {
     const id = req.params.id;
@@ -75,21 +85,14 @@ app.patch('/todos/:id', validatePatch, (req, res, next) => {
 // DELETE Remove
 app.delete('/todos/:id', (req, res, next) => {
   try {
-    const todo = todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
-    if (!todo) return res.status(404).json({ message: 'Todo not found' });
-    Object.assign(todo, req.body); // Merge: e.g., {completed: true}
-    res.status(200).json(todo);
+    const id = parseInt(req.params.id);
+    const initialLength = todos.length;
+    todos = todos.filter((t) => t.id !== id); // Array.filter() – non-destructive
+    if (todos.length === initialLength)
+      return res.status(404).json({ error: 'Not found' });
+    res.status(204).send(); // Silent success
   } catch (error) {
    next(error) 
-  }
-});
-
-app.get('/todos/completed', (req, res, next) => {
-  try {
-    const completed = todos.filter((t) => !t.completed);
-    res.json(completed); // Custom Read!
-  } catch (error) {
-    next(error)
   }
 });
 
