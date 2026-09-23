3535f4271c37e16ba9830f1d26209f9b29dab378
Michael Akelemor <michaelakelemor@gmail.com>
2026-09-23 22:45:08 +0100

    updated the error handler

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-    console.error(err.mesaage);
+    console.error(err.message);
     const status = err.status || 500;
     res.status(status).json({error: err.message});
 };
